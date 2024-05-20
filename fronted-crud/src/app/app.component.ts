8a593cb276e71ae5a9e3a42b168a9f3d4042d4d9
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2024-05-20 01:03:09 -0500

    feat: :art: Se añadio efecto de fade in

--- fronted-crud/src/app/app.component.ts
+++ fronted-crud/src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RedirectService } from './service/redirect.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'fronted-crud';
+  showSplash = true;
 
-  constructor(private redirectService: RedirectService) {}
+  constructor(private redirectService: RedirectService,private router: Router) {}
 
   ngOnInit() {
     this.redirectService.redirectToSplash();
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showSplash = false;
+      this.router.navigate(['/layout']); // Asegúrate de que la ruta sea correcta
+    }, 2000); // Tiempo que se muestra el splash antes de desaparecer
   }
 }
--- fronted-crud/src/app/layout/layout.component.ts
+++ fronted-crud/src/app/layout/layout.component.ts
@@ -1,13 +1,13 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   private currentSection = 1;
-
+  isVisible = false;
   scrollToSection(sectionId: string): void {
     document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
   }
@@ -33,5 +33,10 @@ export class LayoutComponent {
       this.currentSection--;
       this.scrollToSection(`section${this.currentSection}`);
     }
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isVisible = true;
+    }, 100); // Pequeño retraso para asegurar que la transición se aplique
   }
 }
