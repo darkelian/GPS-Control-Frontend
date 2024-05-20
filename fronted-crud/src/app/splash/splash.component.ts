8946d0997f3baa1628897724c2f8e756c13edb50
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2024-05-20 00:50:42 -0500

    feat: :art: Efecto de fade-out entre el inicio y el home

--- fronted-crud/src/app/splash/splash.component.ts
+++ fronted-crud/src/app/splash/splash.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,12 +6,30 @@ import { Router } from '@angular/router';
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.scss']
 })
-export class SplashComponent implements OnInit {
-  constructor(private router: Router) { }
+export class SplashComponent implements OnInit, OnDestroy {
+  isVisible = true;
+  timeoutId: any;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.router.navigate(['/home']);
-    }, 3000); // 3000 ms = 3 segundos
+    this.timeoutId = setTimeout(() => {
+      this.hideSplash();
+    }, 3000); // Tiempo que se muestra la animación antes de desaparecer
+  }
+
+  hideSplash(): void {
+    const splashContainer = document.querySelector('.splash-container');
+    if (splashContainer) {
+      splashContainer.classList.add('fade-out');
+      setTimeout(() => {
+        this.isVisible = false;
+        this.router.navigate(['/home']); // Redirige a la ruta principal después de la transición
+      }, 1500); // Tiempo de la transición en milisegundos
+    }
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.timeoutId);
   }
 }
