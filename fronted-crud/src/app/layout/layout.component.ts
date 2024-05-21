930cee2a7326484517b37285396ea746c740116d
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2024-05-20 22:53:36 -0500

    feat: :art: Se añade animación en los botones que van ayudar hacer el cambio entre secciones de la página

--- fronted-crud/src/app/layout/layout.component.ts
+++ fronted-crud/src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -8,6 +8,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class LayoutComponent implements OnInit {
   private currentSection = 1;
   isVisible = false;
+  constructor(private renderer: Renderer2) { }
   scrollToSection(sectionId: string): void {
     document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
   }
@@ -22,9 +23,11 @@ export class LayoutComponent implements OnInit {
   }
 
   scrollDown(): void {
-    if (this.currentSection < 3) {
+    if (this.currentSection < 2) {
       this.currentSection++;
       this.scrollToSection(`section${this.currentSection}`);
+      this.changeOpacity('.logo-button-up', 1);
+      this.changeOpacity('.logo-button-down', 0);
     }
   }
 
@@ -32,8 +35,20 @@ export class LayoutComponent implements OnInit {
     if (this.currentSection > 1) {
       this.currentSection--;
       this.scrollToSection(`section${this.currentSection}`);
+      this.changeOpacity('.logo-button-up', 0);
+      this.changeOpacity('.logo-button-down', 1);
+
     }
   }
+
+  changeOpacity(selector: string, opacity: number): void {
+    const element = document.querySelector(selector);
+    if (element) {
+      this.renderer.setStyle(element, 'opacity', opacity.toString());
+      this.renderer.setStyle(element, 'transition', 'opacity 0.5s ease-in-out');
+    }
+  }
+
   ngOnInit(): void {
     setTimeout(() => {
       this.isVisible = true;
